0c83f3c24f9db1db9a39081928c1ebdd61ecf950
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-11-21 20:28:41 +0530

    Task: Registration module(Pasword Reset) - Card#3, Card#29 - code clear

--- puc/settings.py
+++ puc/settings.py
@@ -120,7 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
@@ -134,4 +133,3 @@ EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
