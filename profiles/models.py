327004ce809d8f24ebd6806f0ff55af17e765ded
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-11-22 18:21:39 +0530

    Task: User management. Profile photo update. Trello Card#44

--- profiles/models.py
+++ profiles/models.py
@@ -5,12 +5,23 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+import os
+# /PROJECT_ROOT/photos/<user_id>/filename
+# image saving path with users ID
+def avatar_path(instance, filename):
+    return os.path.join('static/avatars', str(instance.id), filename)
+
+
 class Profile(models.Model):
     """docstring for Profile"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+
+    avatar = models.ImageField(
+        'profile picture',
+        upload_to = avatar_path, null=True)
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
@@ -20,3 +31,4 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
--- profiles/views.py
+++ profiles/views.py
@@ -9,14 +9,15 @@ from .forms import UserForm, ProfileForm
 @transaction.atomic
 def update_profile(request):
     if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        user_form = UserForm(request.POST, instance = request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance = request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             messages.success(request, ('Your profile was successfully updated!'))
             return redirect('home')
         else:
+            print profile_form.errors
             messages.error(request, ('Please correct the error below.'))
     else:
         user_form = UserForm(instance=request.user)
