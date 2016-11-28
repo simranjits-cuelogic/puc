ab70d420922dcf369bcc9819e1e9bb160268d698
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-11-28 12:33:09 +0530

    Task: User management. Code review #1 - resolved

--- profiles/views.py
+++ profiles/views.py
@@ -8,6 +8,16 @@ from .forms import UserForm, ProfileForm
 
 @transaction.atomic
 def update_profile(request):
+    """
+    this block of code used to update the user information along with it's profile,
+    since we have different models for user and it's profile. That's why using
+    two form instance name 'user_form' and 'profile_form'. After sucessfully
+    validating user_form and profile_form given data is being saved and sucess
+    message is genrated and request is being redirected to landing page and in
+    case of errors in forms redirecting to same url with errors embedded in it.
+    Above execution being done in case of POST type of request. If request is
+    type of GET, simply show the blank forms (user_form, profile_form).
+    """
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance = request.user)
         profile_form = ProfileForm(request.POST, request.FILES, instance = request.user.profile)
@@ -17,7 +27,6 @@ def update_profile(request):
             messages.success(request, ('Your profile was successfully updated!'))
             return redirect('home')
         else:
-            print profile_form.errors
             messages.error(request, ('Please correct the error below.'))
     else:
         user_form = UserForm(instance=request.user)
